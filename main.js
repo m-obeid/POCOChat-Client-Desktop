e2cd4ba83ba6bd747c708818e3501c8ef93318d6
Mohammad Obeid <mobeid9999@gmail.com>
2022-10-16 14:00:40 +0200

    notifications + windows properly

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, shell, BrowserWindow, ipcMain } = require('electron')
+const { app, shell, BrowserWindow, ipcMain, Tray, Menu, globalShortcut, Notification } = require('electron')
 const path = require('path')
+
+let tray;
 
 const createWindow = () => {
   // Create the browser window.
@@ -14,10 +16,61 @@ const createWindow = () => {
     },
     titleBarStyle: (process.platform === 'darwin' ? 'hidden': 'default')
   })
+  if (process.platform === 'darwin') {
+    globalShortcut.register('Command+Q', () => {
+        forceQuit = true;
+        app.quit();
+    })
+  }
+  tray = new Tray(path.join(__dirname, "icons/tray-icon.png"));
+  tray.setToolTip("POCOChat")
+  tray.on('click', (e) => {
+    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+  });
+  tray.on("right-click", ()=>{
+    tray.popUpContextMenu(trayMenu);
+  });
+  let trayMenuTemplate = [
+    {label: "POCOChat Desktop Beta v1", enabled: false},
+    {label: "Leave POCOChat", click: () => { forceQuit = true; app.quit() }}
+  ]
+  const trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
 
   ipcMain.on("anonfiles-url-autosubmit", (evt, url) => {
     mainWindow.webContents.executeJavaScript(`document.querySelector("#message").value="${url}";sendMessage()`);
   });
+
+  ipcMain.on("notification-handle", (evt, lastMsg) => {
+    if (!mainWindow.isVisible() || !mainWindow.isFocused())
+    {
+      if (lastMsg.type === 'text') {
+        const notify = new Notification({title: lastMsg.username, body: lastMsg.html, hasReply: true, subtitle: "New message on POCOChat", silent: true});
+        notify.on('click', (evt) => {
+          mainWindow.show();
+        })
+        notify.on('reply', (evt, reply) => {
+          mainWindow.webContents.executeJavaScript(`document.querySelector("#message").value=\`${reply.replace("`", "\\`")}\`;sendMessage()`);
+        });
+        notify.show();
+      }
+    }
+  });
+
+  ipcMain.on('window-control-press', (evt, control) => {
+    switch (control) {
+      case 'minimize':
+        mainWindow.minimize()
+        break;
+      case 'maximizeMinimize':
+        if (mainWindow.isMaximized())
+          mainWindow.unmaximize();
+        else
+          mainWindow.maximize();
+        break;
+      case 'close':
+        mainWindow.close()
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL('https://chat.poco.ga');
@@ -45,7 +98,22 @@ const createWindow = () => {
       action: 'deny'
     }
   })
+
+  mainWindow.on('minimize',function(event){
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on('close', function (event) {
+    if (forceQuit) return true;
+      event.preventDefault()
+      mainWindow.hide();
+
+      return false;
+  });
 }
+
+let forceQuit = false;
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -57,6 +125,7 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length > 0 && !BrowserWindow.getAllWindows()[0].isVisible()) BrowserWindow.getAllWindows()[0].show()
   })
 })
 
@@ -64,7 +133,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (forceQuit) app.quit(); 
 })
 
 // In this file you can include the rest of your app's specific main process
--- preload.js
+++ preload.js
@@ -1,5 +1,17 @@
 // Electron preload for stuff
+const {contextBridge, ipcRenderer, ipcMain } = require('electron')
 
 window.onload = () => {
     window.document.querySelector(".upper-navbar").setAttribute("style", "-webkit-app-region: drag"); // enable dragging
+     window.document.querySelector(".window-controls").classList.remove("d-none");
 }
+
+contextBridge.exposeInMainWorld(
+    'electron',
+    {
+      notificationHandler: (lastMsg) => ipcRenderer.send('notification-handle', lastMsg),
+      minimize: () => ipcRenderer.send('window-control-press', 'minimize'),
+      toggleMaxMin: () => ipcRenderer.send('window-control-press', 'maximizeMinimize'),
+      close: () => ipcRenderer.send('window-control-press', 'close')
+    }
+)
